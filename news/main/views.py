25fca3a6058379ce9761dcc21d94e2c222685eab
Olga-Pro <oip_@inbox.ru>
2024-06-24 10:04:56 +0300

    create

---
+++ news/main/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    data = {
+        'caption': "Мой сайт"
+    }
+    return render(request, 'main/index.html', data)
+
+
+def new(request):
+    return render(request, '/main/new.html')
+
+
+def data(request):
+    return HttpResponse("<h1> Новая страница для данных </h1>")
+
+
+def test(request):
+    return HttpResponse("<h1> Новая страница - test </h1>")
---
+++ news/my_news/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class News_post(models.Model):
+    user_name = models.CharField('Автор новости', max_length=50)
+    title = models.CharField('Название новости', max_length=50)
+    short_description = models.CharField('Краткое описание новости', max_length=200)
+    text = models.TextField('Новость')
+    pub_date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'

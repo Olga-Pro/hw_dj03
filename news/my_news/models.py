19589ee752be2f0a1e2e34c80304933fe24df6d8
Olga-Pro <oip_@inbox.ru>
2024-06-24 10:43:20 +0300

    add user

--- news/my_news/models.py
+++ news/my_news/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class News_post(models.Model):
-    user_name = models.CharField('Автор новости', max_length=50)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, verbose_name='Автор новости' , default=1)
+    # Поле `user` связывает модель `News_post` с моделью `User`.
+    # Параметр `on_delete=models.CASCADE` обеспечивает удаление новостей,
+    # если пользователь будет удален из базы данных.
+    # Параметр `verbose_name` используется для более дружественного отображения
+    # имени поля в административной панели.
+    # ID пользователя по умолчанию
+
     title = models.CharField('Название новости', max_length=50)
     short_description = models.CharField('Краткое описание новости', max_length=200)
     text = models.TextField('Новость')
